155ece3e53afff5cd29b39153fa7d9115ab19e43
Hues <saibabukam@gmail.com>
2019-08-31 12:23:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cypress:
+
+npm install cypress
+
+## Running end-to-end tests
+
+node_modules\.bin\cypress open
+
+## Further help
+
+https://www.cypress.io
--- cypress/integration/bdms/view_client.js
+++ cypress/integration/bdms/view_client.js
@@ -3,7 +3,9 @@
 // -- view client page --
 
 it('View Client test', () => {
-    cy.visit('/client/view-client');    
+    cy.visit('/client/view-client');
+    cy.wait(3000);
+    cy.get('.btn-primary').click();
 });
 
 // -- End: Our Cypress Tests --
