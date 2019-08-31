b96f3682c3284d0e3fde3da05a9b9cc977fcfb62
Hues <saibabukam@gmail.com>
2019-08-31 11:22:51 +0530

    first commit

--- cypress/integration/bdms/logout.js
+++ cypress/integration/bdms/logout.js
@@ -1,9 +1,9 @@
-beforeEach(() => {
-    cy.fixture('user').as('userObj');
-});
+// beforeEach(() => {
+//     cy.fixture('user').as('userObj');
+// });
 
-it('Logout test', () => {
-    cy.wait(1500);
-    cy.get('img.ng-star-inserted').click();
-    cy.get('.profile__menu > :nth-child(3) > a').click();
-});
+// it('Logout test', () => {
+//     cy.wait(1500);
+//     cy.get('img.ng-star-inserted').click();
+//     cy.get('.profile__menu > :nth-child(3) > a').click();
+// });
---
+++ cypress/integration/bdms/view_client.js
@@ -0,0 +1,5 @@
+// view client page
+
+it('View Client test', () => {
+    cy.visit('/client/view-client');    
+});
