a35979a7c6548d973a2900daf0fda589e7a7c117
Hues <saibabukam@gmail.com>
2019-09-02 11:27:06 +0530

    first commit

--- cypress/integration/bdms/login.js
+++ cypress/integration/bdms/login.js
@@ -3,14 +3,17 @@
 // Import credentials from fixture
 import userObj from '../../fixtures/user';
 
-beforeEach(function() {
-    cy.fixture('user').as('userObj');
-});
+describe('Test Project', function () {
+    beforeEach(function () {
+        cy.fixture('user').as('userObj');
+    });
 
-it('Login Test', () => {
-    cy.visit('/login');
-    cy.get('#mat-input-0').type(userObj['Username']);
-    cy.get('#mat-input-1').type(userObj['Password']);
-    cy.get('.btn-primary').click();
-    cy.wait(1500);
+    it('Login Test', function () {
+        cy.visit('/login');
+        cy.get('#mat-input-0').type(userObj['Username']);
+        cy.get('#mat-input-1').type(userObj['Password']);
+        cy.get('.btn-primary').click();
+        cy.wait(1500);
+    });
+
 });
