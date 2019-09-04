1d66ede9a788f65a6a15a587ce718b5a0af8ac1c
Hues <saibabukam@gmail.com>
2019-09-04 14:34:03 +0530

    first commit

--- cypress/integration/bdms/view_client.spec.js
+++ cypress/integration/bdms/view_client.spec.js
@@ -1,16 +1,23 @@
 // -- Start: Our Cypress Tests --
 
 describe('View Client page', function () {
+    beforeEach(function () {
+        cy.login();
+    });
+
+    it('visits the form', function () {
+        cy.visit('/client/view-client');
+    });
 
     it('Add new client', function () {
-        cy.visit('/client/view-client');
-        cy.wait(3000);
-        cy.get('.btn-primary');
         cy.get('.btn-primary').click({ force: true });
-        cy.get('#mat-input-3').type('client');
+    });
 
+    it('requires Billing Name', function () {
+        cy.get('#mat-input-3').click('client');
     });
 
 });
+
 
 // -- End: Our Cypress Tests --
