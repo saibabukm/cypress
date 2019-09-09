2d4474e7a3c8421a609db4e3aa99b754973bd3d0
Hues <saibabukam@gmail.com>
2019-09-09 16:04:40 +0530

    first commit

--- cypress/integration/bdms/worksheet_spec.js
+++ cypress/integration/bdms/worksheet_spec.js
@@ -4,6 +4,15 @@ describe('Worksheet page', function () {
     it('Update Worksheet', function () {
         cy.login();
         cy.visit('/workflow/worksheet-module');
-
+        cy.get('#mat-tab-label-0-1').click();
+        cy.wait(3000);
+        cy.get(':nth-child(1) > a > span').click();
+        cy.get(':nth-child(1) > .ng-select > .ng-select-container > .ng-value-container > .ng-input > .ng-star-inserted').click().type('Lev Lloyd{enter}');
+        cy.get('#mat-tab-content-0-1').scrollTo('bottom');
+        cy.scrollTo('0', '100%');
+        cy.wait(2000);
+        cy.get('.btn-primary').click({force: true});
+        cy.get('#mat-tab-content-0-1').scrollTo('top');
+        cy.scrollTo('0', '0%');
     });
 });
