25b50cd2892d7542d94cab4121948d0654774823
Hues <saibabukam@gmail.com>
2019-09-16 13:54:33 +0530

    first commit

--- cypress/integration/bdms/client/view_client_update_spec.js
+++ cypress/integration/bdms/client/view_client_update_spec.js
@@ -5,13 +5,60 @@ describe('Update Client page', function () {
         cy.login();
         cy.visit('/client/view-client');
         cy.get('#mat-input-0').click();
-        cy.get('#mat-input-0').type('Neville Cain{enter}');
-        cy.get('[aria-describedby="cdk-describedby-message-68"] > .material-icons').contains('edit').click();
+        cy.get('#mat-input-0').type('Leah Lee{enter}');
+        cy.get('[aria-describedby="cdk-describedby-message-48"] > .material-icons').contains('edit').click();
 
         // Software
         cy.get('#mat-tab-label-1-1 > .mat-tab-label-content').click();
         cy.get('.panel-title').contains('Update');
+
+        cy.get('#mat-select-14').click();
+        cy.get('#mat-option-66').click();
+
+        cy.get('#mat-input-13').type('79456');
+        cy.get('#mat-input-14').type('123123');
+        cy.get('#mat-input-15').type('user');
+
+        cy.get('#mat-select-15').click();
+        cy.get('#mat-option-85').click();
+
+        cy.get('#mat-input-16').type('123');
+
+        cy.get('#mat-select-16').click();
+        cy.get('#mat-option-103').click();
+
+        cy.get('#mat-select-17').click();
+        cy.get('#mat-option-108').click();
+
+        cy.get('#mat-select-18').click();
+        cy.get('#mat-option-118').click();
+        cy.get('.cdk-overlay-backdrop').click();
+
+        cy.get('body').find('[class="tox-edit-area"]');
+
+        cy.scrollTo(0, 0);
+
+        cy.get('#mat-tab-label-1-4 > .mat-tab-label-content').click();
+
+        cy.get('#mat-select-19').click();
+        cy.get('#mat-option-152').click();
+
+        cy.get('#mat-input-17').type('2020');
+
+        cy.get('#mat-select-20').click();
+        cy.get('#mat-option-155').click();
+
+        cy.get('#mat-input-18').type('789456123');
+
+        cy.get("iframe").then(function ($iframe) {
+            const $body = $iframe.contents().find('body');
+
+            cy.wrap($body).find('p').clear();
+            cy.wrap($body).find('p').type("Hello Tester", { force: true });
+        });
+
     });
 });
+
 
 // -- End: Our Cypress Tests --
