9e68b6f8e02da0998779153bc4a788bf3c40fbe9
Hues <saibabukam@gmail.com>
2019-09-18 13:55:31 +0530

    first commit

--- cypress/integration/dbms/billing/in_spec.js
+++ cypress/integration/dbms/billing/in_spec.js
@@ -3,16 +3,16 @@
 describe('In Info page', function () {
     it('Add In', function () {
         cy.login();
-        cy.visit('/billing/invoices/dashboard');
-        cy.get('.with-button-container > :nth-child(3)').click({force: true} );
-        
+        cy.visit('/billing/invoices/new-invoice');
+
         // Panel Title
         cy.get('.panel-title');
-        cy.get('#mat-input-7').type('18-09-2019');
-        cy.get('#mat-input-8').type('18-09-2019');
+        cy.get('#mat-input-0').type('15-09-2019');
+        cy.get('#mat-input-1').type('18-09-2019');
         cy.get('.mat-select-placeholder').click();
-        cy.get('#mat-option-23 > .mat-option-text').click();
-        cy.get('.ng-input > .ng-star-inserted').click().type('Leah Lee',{enter});
+        cy.get('#mat-option-0').click();
+        cy.get('.ng-input > .ng-star-inserted').type('Leah Lee{enter}');
+        cy.get('.btn-primary');             
 
     });
 });
